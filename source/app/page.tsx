d17c2ee186449f162152f629fbd8441a6109b5b2
itzaRinna <dutchismahdick@gmail.com>
2024-06-27 11:43:32 +0700

    change the structure again

---
+++ source/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-white text-3xl mx-10 my-10 font-bold">Hello World</h1>
+    </>
+  );
+}
--- source/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="text-white text-3xl font-bold">Code here</h1>
-    </>
-  )
-}
